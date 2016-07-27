6a89616fc0a50871f9377cc0261269cd3e64aabc
Steven <chengxd3@gamil.com>
2016-07-27 11:59:52 +0800

    update demo code and image

--- DxMDTextField/ViewController.swift
+++ DxMDTextField/ViewController.swift
@@ -16,24 +16,26 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let nameTextField : MDTextField = MDTextField.init(frame: CGRectMake(15, 86, 280, 64));
+        let width = CGRectGetWidth(self.view.frame)-40;
+        
+        let nameTextField : MDTextField = MDTextField.init(frame: CGRectMake(20, 86, width, 64));
         nameTextField.placeholder = "UserName";
         self.view.addSubview(nameTextField);
         
-        mobileTextField = MDTextField.init(frame: CGRectMake(15, 160, 280, 64));
+        mobileTextField = MDTextField.init(frame: CGRectMake(20, 160, width, 64));
         mobileTextField.placeholder = "Mobile";
         mobileTextField.showMaxInputLength = true;
         mobileTextField.maxInputLength = 18;
         self.view.addSubview(mobileTextField);
         
         
-        let passwordTextField : MDTextField = MDTextField.init(frame: CGRectMake(15, 240, 280, 64));
+        let passwordTextField : MDTextField = MDTextField.init(frame: CGRectMake(20, 240, width, 64));
         passwordTextField.placeholder = "password";
         passwordTextField.secureTextEntry = true;
         self.view.addSubview(passwordTextField);
         
         let btn = UIButton.init(type: UIButtonType.System);
-        btn.frame = CGRectMake(15, 320, 280, 40);
+        btn.frame = CGRectMake(20, 320, width, 40);
         btn.setTitle("OK", forState: UIControlState.Normal);
         btn.addTarget(self, action:#selector(btnPressed), forControlEvents: UIControlEvents.TouchUpInside);
         self.view.addSubview(btn);
